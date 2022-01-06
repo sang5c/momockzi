ac7f8a06384fe9da65335c23d6394415198a8dd2
sang5c <skah321@gmail.com>
2022-01-06 18:30:46 +0900

    init

---
+++ src/main/java/me/sang5c/momockzi/MomockziApplication.java
@@ -0,0 +1,13 @@
+package me.sang5c.momockzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomockziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomockziApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/sang5c/momockzi/MomockziApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sang5c.momockzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomockziApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
